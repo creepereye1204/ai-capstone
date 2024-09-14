7925f5b6e9fb074235bfed4cf2c9b1901d921768
Creeper Eye <iostream1204@gmail.com>
2024-09-14 18:26:19 +0900

    Commit on $(date '+m-H:S')

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -7,18 +7,25 @@ const Navbar = () => {
 
   return (
     <nav>
-      <Link to="/about" className={location === '/about' ? 'onClicked' : 'Link'}>
-        학부소개
-      </Link>
-      <Link to="/track" className={location === '/track' ? 'onClicked' : 'Link'} id="track">
-        교육과정
-      </Link>
-      <Link to="/guide" className={location === '/guide' ? 'onClicked' : 'Link'} id="guide">
-        학사가이드
-      </Link>
-      <Link to="/community" className={location === '/community' ? 'onClicked' : 'Link'} id="community">
-        커뮤니티
-      </Link>
+      <div>
+      <img src="https://www.syu.ac.kr/aice/wp-content/themes/syu-hakbu/images/logo/logo_w.png" alt="Logo" />
+      <Link to="/" className='Link'>인공지능융합학부</Link>
+      </div>
+      
+      <div>
+        <Link to="/about" className={location === '/about' ? 'onClicked' : 'Link'}>
+          학부소개
+        </Link>
+        <Link to="/track" className={location === '/track' ? 'onClicked' : 'Link'} id="track">
+          교육과정
+        </Link>
+        <Link to="/guide" className={location === '/guide' ? 'onClicked' : 'Link'} id="guide">
+          학사가이드
+        </Link>
+        <Link to="/community" className={location === '/community' ? 'onClicked' : 'Link'} id="community">
+          커뮤니티
+        </Link>
+      </div>
     </nav>
   );
 };
--- frontend/src/pages/exam/ch1/exam02.jsx
+++ frontend/src/pages/exam/ch1/exam02.jsx
@@ -1,39 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./exam02.css";
 import Navbar from "../../../components/navbar/navbar";
 import CopyRight from "../../../components/copyRight/copyRight";
-
+import { FaCircle } from 'react-icons/fa';
+import { MdHome } from "react-icons/md";
 const Exam02=()=>{
     return (
-        <div className="header">
+        <div id="main-container">
+        <div id="header-container">
         <Navbar/>
-        <header>
+
         <h1>Division of<br/>
         Artificial Intelligence Convergence</h1>
         <p>4차 산업혁명시대, 창의적 융합 인재를 양성</p>
         
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        
-        <div className="card">
-            
-        </div>
+
         
         
-        <div>
-            <h3>1. AI, Machine Learning, Deep Learning</h3>
-            <p>AI, Machine Learning, Deep Learning은 AI를 이해하고, 제어, ��지, 예��, 학습, 분석, 이해, 추��, 다��기 위한 다��한 학문, 이��, 방법��, 기��, 기법, and tools. </p>
-        </div>
+        
+        
+        
             
         
-        </header>
-        <footer>
-            <CopyRight/>
-        </footer>
+
+
         
+        
+        
+            
+            
+        </div>
+        <div id="card">
+            <MdHome className="home-icon"/>
+            
+            <Link to="/" className="item">Home</Link>       
+            <FaCircle className="dot-icon"/>
+            <Link to="/" className="item">학부소개</Link>  
+            <FaCircle className="dot-icon"/>
+            <Link to="/" className="item">학부소개</Link>       
+        </div>
+        <h2 id="sub-title">학부소개</h2>
+        <div id="middle-cotainer"></div>
         </div>
     );
 };
