5aecd76ddf329f8a824f960c20a05eb45f450595
Creeper Eye <iostream1204@gmail.com>
2024-09-15 21:00:39 +0900

    Commit on $(date '+m-H:S')

--- frontend/src/pages/exam/ch1/exam02.jsx
+++ frontend/src/pages/exam/ch1/exam02.jsx
@@ -41,7 +41,22 @@ const Exam02=()=>{
             <Link to="/" className="item">학부소개</Link>       
         </div>
         <h2 id="sub-title">학부소개</h2>
-        <div id="middle-cotainer"></div>
+        <div id="middle-cotainer">
+            <p id="introduce-title">
+            패러다임 변화와 인공지능기술을<br/>
+
+선도하는 <span id="point-title">글로벌 정보기술학부</span>
+            </p>
+            
+        </div>
+        
+        <div id="footer-container">
+            <img id="aicen" src="https://www.syu.ac.kr/aicen/wp-content/uploads/sites/94/2022/11/ch_ai_3_big.png"></img>
+            <div id="tmi">정보통신 및 인공지능 기술의 발전은 빠른 변화를 야기하고 있으며 기존 사고의 틀을 깨는 다양한 
+                <span id="emerald">&nbsp;창의적 시도</span>를 통하여<span id="blue">&nbsp;패러다임의 변화를</span>추구하고있다.
+                <p id="tmi-sub-grey">이러한 발전과 변화의 추세는 다양한 분야에서 새로운 인공지능 기반의 정보 기술 도입을 요구하고 있으며, 이를 효율적으로 진행하고 활용하는 것이 경쟁전략의 핵심으로 자리 잡게 되었다. 또한, 정형, 비정형 빅데이터 기반 의사결정의 중요성이 급격히 증대됨에 따라 각 분야의 지능 정보 기술에 대한 전문 지식과 경영적 마인드를 갖춘 융합형 인재에 대한 수요가 증가되고 있다. 인공지능융합학부에서는 인공지능 중심의 공학적 전문 역량 함양을 기본으로 인문사회학적 소양과 경영적 통찰력을 갖춘 전문 인재 양성을 목표로 하고 있다.</p>
+            </div>
+        </div>
         </div>
     );
 };
