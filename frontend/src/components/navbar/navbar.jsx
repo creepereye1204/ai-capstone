dbcf85ec44779bd22751ddf6a45f81fb8f5b0e7e
creepereye <creepereye1204@gmail.com>
2024-09-17 13:06:56 +0900

    Commit on $(date '+%Y-%m-%d %H:%M:%S')

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -7,22 +7,42 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div>
-      <img src="https://www.syu.ac.kr/aice/wp-content/themes/syu-hakbu/images/logo/logo_w.png" alt="Logo" />
-      <Link to="/" className='Link'>인공지능융합학부</Link>
+      <div id="logo">
+        <img
+          src="https://www.syu.ac.kr/aice/wp-content/themes/syu-hakbu/images/logo/logo_w.png"
+          alt="Logo"
+        />
+        <Link to="/" className="Link">
+          인공지능융합학부
+        </Link>
       </div>
-      
+
       <div>
-        <Link to="/about" className={location === '/about' ? 'onClicked' : 'Link'}>
+        <Link
+          to="/about"
+          className={location === "/about" ? "onClicked" : "Link"}
+        >
           학부소개
         </Link>
-        <Link to="/track" className={location === '/track' ? 'onClicked' : 'Link'} id="track">
+        <Link
+          to="/track"
+          className={location === "/track" ? "onClicked" : "Link"}
+          id="track"
+        >
           교육과정
         </Link>
-        <Link to="/guide" className={location === '/guide' ? 'onClicked' : 'Link'} id="guide">
+        <Link
+          to="/guide"
+          className={location === "/guide" ? "onClicked" : "Link"}
+          id="guide"
+        >
           학사가이드
         </Link>
-        <Link to="/community" className={location === '/community' ? 'onClicked' : 'Link'} id="community">
+        <Link
+          to="/community"
+          className={location === "/community" ? "onClicked" : "Link"}
+          id="community"
+        >
           커뮤니티
         </Link>
       </div>
